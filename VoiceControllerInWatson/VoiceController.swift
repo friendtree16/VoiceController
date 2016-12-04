1da3852bf9584bd68544d0fcee61295ebfccd20b
tomoki katsu <stiino58@gmail.com>
2016-12-04 15:51:59 +0900

    プロジェクト追加

---
+++ VoiceControllerInWatson/VoiceController.swift
@@ -0,0 +1,15 @@
+//
+//  VoiceController.swift
+//  VoiceControllerInWatson
+//
+//  Created by tomoki on 2016/12/04.
+//  Copyright © 2016年 friendTree16. All rights reserved.
+//
+
+import UIKit
+
+class VoiceController: NSObject {
+    public func startControl(comletion: ((_ result:Int,_ error:NSError?) -> Void)) {
+        
+    }
+}
---
+++ VoiceControllerInWatson/VoiceControllerInWatson.h
@@ -0,0 +1,19 @@
+//
+//  VoiceControllerInWatson.h
+//  VoiceControllerInWatson
+//
+//  Created by tomoki on 2016/12/04.
+//  Copyright © 2016年 friendTree16. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VoiceControllerInWatson.
+FOUNDATION_EXPORT double VoiceControllerInWatsonVersionNumber;
+
+//! Project version string for VoiceControllerInWatson.
+FOUNDATION_EXPORT const unsigned char VoiceControllerInWatsonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VoiceControllerInWatson/PublicHeader.h>
+
+
